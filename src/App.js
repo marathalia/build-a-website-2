2889e92c35781dcfdbcbb699ad2d211e9ef7915c
marathalia <marathalia@users.noreply.github.com>
2020-12-29 10:57:40 +0700

    added picture and changed text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import logo512 from './assets/logo512.png';
 
 function App() {
   return (
     <div className="App bg-blue-200 h-screen flex items-center justify-center">
       <header className="App-header">
+      
+        <div>
+          <img src={logo512} />
+        </div>
+    
         <p className="text-2xl">
-          Edit <code>src/Home.js</code> and save to reload.
+          Hello Everyone!
         </p>
         <a
           className="App-link text-sm font-light"
