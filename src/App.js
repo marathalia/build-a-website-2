d73077db1fabdd68fbfa3954fe4b9edde685337c
marathalia <marathalia@users.noreply.github.com>
2020-12-30 12:11:50 +0700

    made a new component City.js and modified Home.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 
 import Home from "./containers/Home";
 // you always need the ./ when importing something in js
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,10 +5,19 @@ import React, { useState, useEffect, useMemo } from "react";
 import Header from "../components/Header";
 import WeatherImage from "../components/WeatherImage";
 
+import City from "../components/City";
 const weatherKey = `TODO`; // Your API Key here
 
 function Home() {
   // TODO
+  return (
+    // Container
+    <div className="flex flex-col h-screen bg-green-200">
+      <City cityName={"Tokyo"} temp={"10°C"} color={"bg-yellow-500"} />
+      <City cityName={"Jakarta"} temp={"20°C"} color={"bg-red-500"} />
+      <City cityName={"Bandung"} temp={"30°C"} color={"bg-blue-500"} />
+    </div>
+  );
 }
 
 export default Home;
